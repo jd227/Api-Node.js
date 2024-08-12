2d46810d447e5f85ababd54176baffc5d4eea016
jd227 <jd821jd@gmail.com>
2024-08-12 13:03:18 -0500

    New folder Route  correction API

--- server/app.js
+++ server/app.js
@@ -1,14 +1,14 @@
 import express from "express";
 import Routes from "./Routes/route.js";
 import Cors from "cors";
-
 const App = express();
+const port = 8000;
 
 App.use(Cors());
 App.use(express.json());
 App.use(Routes);
 
-const port = 8000;
+
 
 App.listen(port,()=>{
     console.log(`http://localhost:${port}`);
